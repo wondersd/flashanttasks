f3487da9a8e1c1707cd9808da7a689f93aa678bd
jodybrewster <jodybrewster@1e683660-3267-11de-a996-7ff70a941223>
2009-04-26 13:42:42 +0000

    Initial Commit

---
+++ trunk/src/net/jodybrewster/ant/exceptions/FlashException.java
@@ -0,0 +1,9 @@
+package net.jodybrewster.ant.exceptions;
+
+public class FlashException extends Exception {
+
+	public FlashException(String err)
+	{
+		super(err);
+	}
+}
---
+++ trunk/src/net/jodybrewster/ant/taskdefs/FlashCommand.java
@@ -0,0 +1,123 @@
+package net.jodybrewster.ant.taskdefs;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+
+/**
+ * Wrapper for the rest of the Movie tasks
+ * 
+ * @author Jody Brewster
+ *
+ */
+public class FlashCommand extends Task {
+	
+	private String TEMP_DIR = "tmp_flashcommand";
+	
+	private List<Movie> _movies = new ArrayList<Movie>();
+	private Boolean _failOnError = false;
+	private Boolean _logError = false;
+	private Boolean _verbose = false;
+	private String _flashVersion = "CS4";
+	
+	/**
+	 * Executes the ant task
+	 */
+	public void execute() throws BuildException
+	{
+		if (_verbose)
+			System.out.println("FlashCommand logError=" + _logError + " failOnError=" + _failOnError);
+	
+		for (Movie m : _movies)
+		{
+			try
+			{
+				createTempDirectory();
+				m.execute();
+				cleanUp();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+				System.exit(0);
+			}
+			
+		}
+	}
+	
+	/**
+	 * Creates a temp directory to hold all of the logs
+	 * @throws IOException
+	 */
+	private void createTempDirectory() throws IOException
+	{
+		if (_verbose)
+			System.out.println("Creating the temp dir: " + TEMP_DIR);
+		
+		java.io.File dir = new java.io.File(TEMP_DIR);
+		dir.mkdir();
+		
+		
+	}
+	
+	/**
+	 * Deletes the temp directory
+	 * 
+	 * @throws IOException
+	 */
+	private void cleanUp() throws IOException
+	{
+		if (_verbose)
+			System.out.println("Deleting the temp dir: " + TEMP_DIR);
+		
+		java.io.File dir = new java.io.File(TEMP_DIR);
+		Boolean d = dir.delete();
+		
+		if (_verbose)
+		{
+			if (d)
+				System.out.println("Temp dir deleted: " + TEMP_DIR);
+			else
+				System.out.println("Could not delete temp dir: " + TEMP_DIR);
+		}
+	}
+	
+	
+	public void add(Movie f)
+	{
+		f.setVerbose(_verbose);
+		f.setVersion(_flashVersion);
+		
+		_movies.add(f);
+	}
+	
+	public void setFailonerror(Boolean x)
+	{
+		_failOnError = x;
+	}
+	
+	public void setLogError(Boolean x)
+	{
+		_logError = x;
+	}
+	
+
+	
+	public void setVerbose(Boolean x)
+	{
+		_verbose = x;
+	}
+	
+	
+	public void setFlashversion(String x)
+	{
+		if (x.toUpperCase() != "CS4" || x.toUpperCase() != "CS3")
+			x = "flash";
+			
+		_flashVersion = x.toUpperCase();
+	}
+}
